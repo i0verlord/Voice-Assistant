d9742ae444c895fcb6515fbf37c5de61499fb9ec
Aman Jain <105035823+i0verlord@users.noreply.github.com>
2023-03-11 15:28:28 +0530

    code sheet

---
+++ Test_Alexa.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait() 
+    wikipedia.summary(person, 1)
